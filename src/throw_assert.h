83ff868cc3cc65b6d7ac33ac2040dd28146bbff2
Jason Hiser <jdhiser@gmail.com>
2019-09-13 15:22:04 -0400

    added option to use throwing assert or real assert

--- src/throw_assert.h
+++ src/throw_assert.h
@@ -1,5 +1,10 @@
 
 
 #ifndef throw_asssert
+
+#ifdef use_throwing_assert
 #define throw_assert(a) { if(!(a)) throw std::invalid_argument(#a); } 
+#else
+#include <assert.h>
+#define throw_assert(a) { assert(a); } 
 #endif
