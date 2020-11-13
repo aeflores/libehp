4ea6f2b76d0fb0e317cea18a652f34b786ecf42b
Jason Hiser <jdhiser@gmail.com>
2020-11-13 09:39:54 -0500

    Update cicd_testing/do-fuzz.sh

--- cicd_testing/do-fuzz.sh
+++ cicd_testing/do-fuzz.sh
@@ -14,7 +14,7 @@ function main()
 
 	# force reinstall tools so we are always up-to-date
 	yes | sudo bash -c "$(curl -fsSL allzp.zephyr-software.io/turbo/cli-install.sh)"
-	sudo echo core >/proc/sys/kernel/core_pattern
+	sudo bash -c 'echo core >/proc/sys/kernel/core_pattern'
 
 	# docker run starts a container.  since we started with --restart, it should come back up
 	# docker run -d --restart unless-stopped -p 55155:55155 git.zephyr-software.com:4567/allzp/turbo/turbo:latest
